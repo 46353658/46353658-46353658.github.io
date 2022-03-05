3673999f99910da4c35f0a5fa746dab150f8b6ee
46353658 <student46353658@gmail.com>
2022-03-05 19:27:13 +0200

    Add files via upload

---
+++ js/coachability.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+  data() {
+    return { 
+      text: 'Coachability quiz',
+      question1: false,
+      question2: false,
+      question3: false,
+      coachabilityResult: ''
+    }
+  }, 
+  methods: {
+    logValue() {
+      console.log(this.question1);
+    }, 
+    checkStatus() {
+      console.log(`${this.question1} - ${this.question2} - ${this.question3} - ${this.question4}`)
+      if (!this.question1) {
+        this.coachabilityResult = 'You have to choose counseling yourself, do not force yourself nor your loved ones into something they are not ready for';
+      } else if (!this.question2) {
+        this.coachabilityResult = 'Self belief is key, if you do not believe in yourself, others cannot believe in you';
+      } else if (!this.question3) {
+        this.coachabilityResult = 'Daily time to reflect and work on yourself is an essential sacrifice for growth.';
+      } else {
+        this.coachabilityResult = 'You are ready!';
+      }
+    }
+  }
+});
+
+app.mount('#coachability');
